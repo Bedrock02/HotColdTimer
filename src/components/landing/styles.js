057c88320d26271807500b01533307517aae53d7
Bedrock02 <jimsteve91@gmail.com>
2019-06-24 13:03:55 -0400

    Snowflake Animation Change - Removed shake horizontal to a shake bottom

--- src/components/landing/styles.js
+++ src/components/landing/styles.js
@@ -1,3 +1,4 @@
+// Animation is from Animista
 const LandingStyles = {
   iconContainer: {
     display: 'flex',
@@ -10,8 +11,8 @@ const LandingStyles = {
 	  animation: 'pulsate-fwd 0.5s ease-in-out infinite both',
   },
   snowflake: {
-    WebkitAnimation: 'shake-horizontal 1s cubic-bezier(0.455, 0.030, 0.515, 0.955) infinite both',
-	  animation: 'shake-horizontal 1s cubic-bezier(0.455, 0.030, 0.515, 0.955) infinite both',
+    WebkitAnimation: 'shake-bottom 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) infinite both',
+	  animation: 'shake-bottom 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) infinite both',
   }
 }
 export default LandingStyles;
